9a453c4c7ae864e9fd50d70dec5362ea552f76c6
Joelson Reis <50884925+joelsondsbreis@users.noreply.github.com>
2020-03-31 22:25:46 -0300

    Página de cadastro finalizada.

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -9,21 +9,27 @@ import logoImg from '../../assets/logo.svg'
 export default function Register() {
     return (
         <div className="register-container">
-            <div className="Content">
+            <div className="content">
                 <section>
                     <img src={logoImg} alt="Be The Hero" />
 
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
 
-                    <Link className="back-link" to="/register">
+                    <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041" />
-                         Cadastrar
+                         Não tenho cadastro
                         </Link>
                 </section>
-
                 <form>
-
+                    <input placeholder="Nome da ONG" />
+                    <input type="email" placeholder="E-mail" />
+                    <input placeholder="WhatsApp" />
+                        <div className="input-group">
+                            <input placeholder="Cidade" />
+                            <input placeholder="UF" style={{ width: 80 }} />
+                        </div>
+                    <button className="button" type="submit">Cadastrar</button>
                 </form>
             </div>
         </div>
